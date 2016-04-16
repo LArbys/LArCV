d67ddac3725e8ea8343da9e257925581bc1495ad
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-04-16 02:44:19 -0400

    stupid img dump when rgb viewer not working ;) --kazu

---
+++ mac/dump_img.py
@@ -0,0 +1,26 @@
+from ROOT import larcv
+larcv.load_pycvutil
+larcv.IOManager
+import matplotlib.pyplot as plt
+from ROOT import TChain
+import sys
+
+IMAGE_PRODUCER=sys.argv[1]
+
+img_tree_name='image2d_%s_tree' % IMAGE_PRODUCER
+img_br_name='image2d_%s_branch' % IMAGE_PRODUCER
+img_ch = TChain(img_tree_name)
+img_ch.AddFile(sys.argv[2])
+
+for entry in xrange(img_ch.GetEntries()):
+    img_ch.GetEntry(entry)
+    img_br=None
+    exec('img_br=img_ch.%s' % img_br_name)
+    event_key = img_br.event_key()
+    for img in img_br.Image2DArray():
+        mat=larcv.as_ndarray(img)
+        mat_display=plt.imshow(mat)
+        mat_display.write_png('%s_plane%d.png' % (event_key,img.meta().plane()))
+
+
+
