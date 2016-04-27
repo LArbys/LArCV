318d942acb639475069376e74bbe17bb6a38b515
kazuhiro <kazuhiro@nevis.columbia.edu>
2016-04-26 22:50:30 -0400

    N planes! --kazu

--- app/tools/calculate_mean.cc
+++ app/tools/calculate_mean.cc
@@ -32,10 +32,8 @@ int main(int argc, char** argv){
   output_strm.set_out_file("mean.root");
   output_strm.initialize();
   auto output_image = (larcv::EventImage2D*)(output_strm.get_data(larcv::kProductImage2D,"mean"));
-  std::map<larcv::PlaneID_t,std::vector<double> > image_m;
-  std::map<larcv::PlaneID_t,larcv::ImageMeta > meta_m;
+  std::vector<larcv::Image2D> image_v;
 
-  const size_t NPLANES=3;
   size_t nentries = input_strm.get_n_entries();
   size_t entries_fraction = nentries/10;
   size_t entry=0;
@@ -50,60 +48,33 @@ int main(int argc, char** argv){
 
     auto event_image = (larcv::EventImage2D*)(input_strm.get_data(larcv::kProductImage2D,producer));
 
-    if(event_image->Image2DArray().size()!=NPLANES) continue;
-    
-    if(image_m.empty()) {
-      
-      for(auto const& img : event_image->Image2DArray()) {
+    if(image_v.empty()) {
 
-	auto const& vec = img.as_vector();
-	auto plane = img.meta().plane();
-	meta_m[plane] = img.meta();
-	std::vector<double> vec_copy(vec.size(),0.);
-	for(size_t i=0; i<vec.size(); ++i) vec_copy[i] = vec[i];
-	image_m.emplace(plane,std::move(vec_copy));
-      }
-      
-      if(image_m.size() != NPLANES) {
-	LARCV_CRITICAL() << "Duplicate plane? # plane-image does not make sense..." << std::endl;
-	throw larcv::larbys();
-      }
+      event_image->Move(image_v);
 
     }else{
 
-      for(auto const& img : event_image->Image2DArray()) {
+      if(image_v.size() != event_image->Image2DArray().size())
 
-	auto iter = image_m.find(img.meta().plane());
-	if(iter == image_m.end()) {
-	  LARCV_CRITICAL() << "PlaneID_t set changed during the event loop... unexpected..." << std::endl;
-	  throw larcv::larbys();
-	}
-	auto& vec_copy = (*iter).second;
-	auto const& vec = img.as_vector();
-	if(vec.size() != vec_copy.size()) {
-	  LARCV_CRITICAL() << "Image size has changed for plane " << (*iter).first << std::endl;
-	  throw larcv::larbys();
-	}
-	
-	for(size_t i=0; i<vec.size(); ++i) vec_copy[i] += vec[i];
+	throw ::larcv::larbys("Incompatible length of Image2DArray found!");
+
+      auto const& in_image_v = event_image->Image2DArray();
+
+      for(size_t i=0; i<in_image_v.size(); ++i) {
+
+	image_v[i] += in_image_v[i].as_vector();
+
       }
     }
     ++image_count;
   }
 
-  for(auto& plane_img : image_m) {
+  for(auto& img : image_v)
 
-    auto const& plane = plane_img.first;
-    auto& vec_copy = plane_img.second;
-    for(auto& v : vec_copy) v /= image_count;
-    
-    std::vector<float> vec(vec_copy.size(),0);
-    for(size_t i=0; i<vec.size(); ++i) vec[i] = (float)(vec_copy[i]);
+    img /= image_count;
 
-    larcv::ImageMeta meta(meta_m[plane]);
-    larcv::Image2D img(std::move(meta),std::move(vec));
-    output_image->Emplace(std::move(img));
-  }
+  output_image->Emplace(std::move(image_v));
+
   output_strm.set_id(0,0,0);
   output_strm.save_entry();
   output_strm.finalize();
