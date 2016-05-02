c01e18c6ea27f4fafdb9b19c9ccb0f73d3d9ab93
Victor Genty <vgenty@nevis.columbia.edu>
2016-05-02 01:26:52 -0500

    gaussian blur from cv2 goes in.........

--- mac/pyrgb/rgb_cv2/cv2selector.py
+++ mac/pyrgb/rgb_cv2/cv2selector.py
@@ -2,13 +2,15 @@ from cv2blur import CV2Blur
 from cv2nothing import CV2Nothing
 from cv2add import CV2Add
 from cv2multiply import CV2Multiply
+from cv2gausblur import CV2GausBlur
 
 class CV2Selector(object):
         def __init__(self):
 		self.selections = { "nothing"  : CV2Nothing(),
                                     "blur"     : CV2Blur() ,
                                     "add"      : CV2Add(),
-                                    "multiply" : CV2Multiply()}
+                                    "multiply" : CV2Multiply(),
+                                    "gausblur" : CV2GausBlur()}
                 
                 self.selection = None
 
