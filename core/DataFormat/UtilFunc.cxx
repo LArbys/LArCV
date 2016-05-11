679c7bab8de04c5fb04122867c77cb6ce9865bda
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-05-11 17:34:55 -0400

    added a function to map ROIType_t to/from string --kazu

--- core/DataFormat/UtilFunc.cxx
+++ core/DataFormat/UtilFunc.cxx
@@ -2,6 +2,7 @@
 #define __DATAFORMAT_UTILFUNC_CXX__
 
 #include "UtilFunc.h"
+#include "Base/larbys.h"
 namespace larcv {
 
   ROIType_t PDG2ROIType(const int pdgcode)
@@ -41,6 +42,47 @@ namespace larcv {
     default:
       return kROIUnknown;
     }
+    return kROIUnknown;
+  }
+
+  std::string ROIType2String(const ROIType_t type) 
+  {
+    switch(type) {
+    case kROIUnknown: return "Unknown";
+    case kROICosmic:  return "Cosmic";
+    case kROIBNB:     return "BNB";
+    case kROIEminus:  return "Eminus";
+    case kROIGamma:   return "Gamms";
+    case kROIPizero:  return "Pizero";
+    case kROIMuminus: return "Muminus";
+    case kROIKminus:  return "Kminus";
+    case kROIPiminus: return "Piminus";
+    case kROIProton:  return "Proton";
+    default:
+      std::stringstream ss;
+      ss << "Unsupported type: " << type << std::endl;
+      throw larbys(ss.str());
+    }
+    return "";
+  }
+
+  ROIType_t String2ROIType(const std::string& name)
+  {
+    if(name == "Unknown") return kROIUnknown;
+    if(name == "Cosmic" ) return kROICosmic;
+    if(name == "BNB"    ) return kROIBNB;
+    if(name == "Eminus" ) return kROIEminus;
+    if(name == "Gamma"  ) return kROIGamma;
+    if(name == "Pizero" ) return kROIPizero;
+    if(name == "Muminus") return kROIMuminus;
+    if(name == "Kminus" ) return kROIKminus;
+    if(name == "Piminus") return kROIPiminus;
+    if(name == "Proton" ) return kROIProton;
+    
+    std::stringstream ss;
+    ss << "Unsupported name: " << name << std::endl;
+    throw larbys(ss.str());
+
     return kROIUnknown;
   }
 }
