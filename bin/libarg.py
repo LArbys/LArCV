1c32dc76a312fdd3ee9912092aa3c84ce8c2e38b
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-07-13 10:46:05 -0500

    dependency stuffs resolved --kazu

--- bin/libarg.py
+++ bin/libarg.py
@@ -1,7 +1,11 @@
 
 import sys,os,commands
 
-dirs=[x.replace('-lLArCV','') for x in commands.getoutput('larcvapp-config --libs').split() if x.startswith('-lLArCV')]
+dirs=[]
+for d in os.listdir(os.environ['LARCV_BUILDDIR']):
+    if not len([x for x in os.listdir('%s/%s' % (os.environ['LARCV_BUILDDIR'],d)) if x.endswith('.o')]): continue
+    dirs.append(d)
+#dirs=[x.replace('-lLArCV','') for x in commands.getoutput('larcvapp-config --libs').split() if x.startswith('-lLArCV')]
 libs=[x for x in commands.getoutput('larcvapp-config --libs').split() if not x.startswith('-lLArCV')]
 libs+= commands.getoutput('root-config --libs').split()
 objs_list=[]
@@ -9,16 +13,40 @@ dict_list=[]
 for l in dirs:
     d='%s/%s' % (os.environ['LARCV_BUILDDIR'],l)
     #print d, os.path.isdir(d)
-    objs_list.append(['%s/%s' % (d,x) for x in os.listdir(d) if x.endswith('.o') and not x.endswith('Dict.o')])
-    dict_list.append(['%s/%s' % (d,x) for x in os.listdir(d) if x.endswith('Dict.o')])
+    #print os.listdir(d)
+    src_obj = ['%s/%s' % (d,x) for x in os.listdir(d) if x.endswith('.o') and not x.endswith('Dict.o')]
+    dic_obj = ['%s/%s' % (d,x) for x in os.listdir(d) if x.endswith('Dict.o')]
+
+    if len(src_obj) == 0 and len(dic_obj) == 0: continue
+    
+    objs_list.append(src_obj)
+    dict_list.append(dic_obj)
     #print objs_list[-1]
     #print dict_list[-1]
 
-cmd='-o %s/liblarcv.so ' % os.environ['LARCV_LIBDIR']
+if not dict_list and not objs_list: sys.exit(0)
+
+libname = '%s/liblarcv.so' % os.environ['LARCV_LIBDIR']
+    
+cmd='-o %s ' % libname
+base_ts = None
+if os.path.isfile(libname):
+    base_ts=os.path.getmtime(libname)
+
+skip_build = base_ts is not None
 for objs in objs_list:
 
-    for obj in objs: cmd += '%s ' % obj
+    for obj in objs:
 
+        cmd += '%s ' % obj
+
+        ts = os.path.getmtime(obj)
+
+        if skip_build and ts > base_ts:
+            skip_build = False
+
+if skip_build: sys.exit(0)
+    
 for d in dict_list:
 
     if d: cmd += '%s ' % d[0]
@@ -27,3 +55,4 @@ for l in libs:
     cmd += '%s ' % l
 
 print cmd
+sys.exit(1)
