3bfcf9c76362004dd451052445b906f0e5064a45
Taritree Wongjirad <twongjirad@MacBook-Air.local>
2016-08-04 17:25:27 -0400

    minor udpates to calibadc script.

--- app/CalibADC/count_golden_wires.py
+++ app/CalibADC/count_golden_wires.py
@@ -13,8 +13,11 @@ ngood = {0:0,
          2:0}
 
 scalehists = {}
+peakmeans = {}
 for p in range(0,3):
     scalehists[p] = rt.TH1D("hscale_p%d"%(p),"",100,0,2.0)
+    peakmeans[("mc",p)]  = rt.TH1D("hpeak_mc_p%d"%(p),"", 220, 0, 220)
+    peakmeans[("data",p)]  = rt.TH1D("hpeak_data_p%d"%(p),"", 220, 0, 220)
 
 lchbych = chbych.readlines()
 #['2', '863', '368.49', '181.10', '2.03', '1']
@@ -26,13 +29,19 @@ planes = {0:"U",
 maxwires = {0:2400,1:2400,2:3456}
 for l in lchbych[1:]:
     info =  l.strip().split()
+    datap = float( info[2] )
+    mcp   = float( info[3] )
     scale = float( info[4] )
     badmask = int( info[5] )
     plane = int( info[0] )
     wid   = int( info[1] )
 
+    
+
     if badmask==1:
         scalehists[plane].Fill( scale )
+        peakmeans[("mc",plane)].Fill( mcp )
+        peakmeans[("data",plane)].Fill( datap )
     else:
         scalehists[plane].Fill( 0.0 )
 
@@ -46,13 +55,33 @@ for l in lchbych[1:]:
 badchlist.close()
 
 print "NGOOD:"
-print "U-plane: ",ngood[0]
-print "V-plane: ",ngood[1]
-print "Y-plane: ",ngood[2]
+print "U-plane: ",ngood[0],"(",ngood[0]*4.0/2400.0*100.0,"%%)"
+print "V-plane: ",ngood[1],"(",ngood[1]*4.0/2400.0*100.0,"%%)"
+print "Y-plane: ",ngood[2],"(",ngood[2]*4.0/3456.0*100.0,"%%)"
+
+print "TOTAL: ",ngood[0]+ngood[1]+ngood[2]," (",4.0*(ngood[0]+ngood[1]+ngood[2])/(2400+2400+3456.0)*100.0,"%%)"
+
+# Fit scale distributions
+
+print "SCALE FACTOR PEAK"
+for p in range(0,3):
+    scalehists[p].Fit("gaus","R","",0.3, 1.5)
+
+print "MC PEAKS"
+for p in range(0,3):
+    peakmeans[("mc",p)].Fit("gaus","R","",50, 200)
+
+print "DATA PEAKS"
+for p in range(0,3):
+    peakmeans[("data",p)].Fit("gaus","R","",50, 200)
+
 
 
 c = rt.TCanvas("c","c",800,600)
 c.Draw()
+c.SetLogy(1)
+c.SetGridx(1)
+c.SetGridy(1)
 
 scalehists[0].Draw()
 scalehists[1].Draw("same")
@@ -63,6 +92,22 @@ scalehists[1].SetLineColor(rt.kGreen)
 scalehists[2].SetLineColor(rt.kBlue)
 
 c.Update()
+
+# cp = rt.TCanvas("cp","cp",800,600)
+# cp.Draw()
+# cp.SetLogy(1)
+# cp.SetGridx(1)
+# cp.SetGridy(1)
+
+# scalehists[0].Draw()
+# scalehists[1].Draw("same")
+# scalehists[2].Draw("same")
+
+# scalehists[0].SetLineColor(rt.kRed)
+# scalehists[1].SetLineColor(rt.kGreen)
+# scalehists[2].SetLineColor(rt.kBlue)
+
+# c.Update()
 
 raw_input()
     
