6eea252e923cf45516226dea86e0696bd7b687dd
Erez Cohen <erezcohen@erezs-mbp.guest-network.net>
2016-08-05 10:06:50 -0500

    Erez larcv installation revealed view_rgb was NOT loadin library from larcv python module (instead from bare ROOT). This should help fixing some others' problem as well --kazu

--- python/larcv/__init__.py
+++ python/larcv/__init__.py
@@ -2,6 +2,9 @@ import ROOT,os
 if not 'LARCV_BASEDIR' in os.environ:
     print '$LARCV_BASEDIR shell env. var. not found (run configure.sh)'
     raise ImportError
+#force loading larlite libs FIRST because auto-loading in ROOT6 does not properly work
+if 'LARLITE_BASEDIR' in os.environ:
+    from larlite import larlite
 larcv_dir = os.environ['LARCV_LIBDIR']
 for l in [x for x in os.listdir(larcv_dir) if x.endswith('.so')]:
     ROOT.gSystem.Load(l)
