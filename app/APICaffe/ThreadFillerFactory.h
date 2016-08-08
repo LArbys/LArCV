2be0eadfd46ac986aa0b3aa7f5ea492a5f1cf46b
Victor Genty <vgenty@nevis.columbia.edu>
2016-08-07 23:34:11 -0400

    kazu forgot to commit destroy filler so I steal it out of his repo and commit

--- app/APICaffe/ThreadFillerFactory.h
+++ app/APICaffe/ThreadFillerFactory.h
@@ -13,12 +13,20 @@ namespace larcv {
 
     static bool exist_filler(const std::string& name) {
       auto iter = _filler_m.find(name);
-      return (iter != _filler_m.end());
+      return (iter != _filler_m.end() && (*iter).second);
+    }
+
+    static void destroy_filler(const std::string& name) {
+      if(!exist_filler(name)) return;
+      if(_filler_m[name]) {
+	delete _filler_m[name];
+	_filler_m[name] = nullptr;
+      }
     }
     
     static ThreadDatumFiller& get_filler(const std::string& name) {
       auto iter = _filler_m.find(name);
-      if(iter != _filler_m.end()) return (*((*iter).second));
+      if(iter != _filler_m.end() && (*iter).second) return (*((*iter).second));
       auto ptr = new ::larcv::ThreadDatumFiller(name);
       _filler_m[name]=ptr;
       return (*ptr);
