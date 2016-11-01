a1bf47abc8b586aab3f0a9a4a053bbda3d1a5d6b
Corey Adams <corey.adams@yale.edu>
2016-11-01 10:29:30 -0500

    Modify image ana to use all pixels

--- app/ImageMod/ADCScaleAna.cxx
+++ app/ImageMod/ADCScaleAna.cxx
@@ -6,57 +6,60 @@
 
 namespace larcv {
 
-  static ADCScaleAnaProcessFactory __global_ADCScaleAnaProcessFactory__;
+static ADCScaleAnaProcessFactory __global_ADCScaleAnaProcessFactory__;
 
-  ADCScaleAna::ADCScaleAna(const std::string name)
-    : ProcessBase(name)
-  {}
-    
-  void ADCScaleAna::configure(const PSet& cfg)
-  {
-    _image_producer = cfg.get<std::string>("ImageProducer");
-  }
+ADCScaleAna::ADCScaleAna(const std::string name)
+  : ProcessBase(name)
+{}
 
-  void ADCScaleAna::initialize()
-  {
-    for(auto& p : _hADC_v) delete p;
-    _hADC_v.clear();
-  }
+void ADCScaleAna::configure(const PSet& cfg)
+{
+  _image_producer = cfg.get<std::string>("ImageProducer");
+}
 
-  bool ADCScaleAna::process(IOManager& mgr)
-  {
-    static ProducerID_t id = mgr.producer_id(kProductImage2D,_image_producer);
+void ADCScaleAna::initialize()
+{
+  for (auto& p : _hADC_v) delete p;
+  _hADC_v.clear();
+}
 
-    auto event_image = (EventImage2D*)(mgr.get_data(id));
+bool ADCScaleAna::process(IOManager& mgr)
+{
+  static ProducerID_t id = mgr.producer_id(kProductImage2D, _image_producer);
 
-    for(auto const& img : event_image->Image2DArray()) {
+  auto event_image = (EventImage2D*)(mgr.get_data(id));
 
-      auto const plane = img.meta().plane();
-      if(_hADC_v.size() <= plane) _hADC_v.resize(plane+1,nullptr);
+  for (auto const& img : event_image->Image2DArray()) {
 
-      if(!_hADC_v[plane]) _hADC_v[plane] = new TH1D(Form("hADC_%s_Plane%02hu",_image_producer.c_str(),plane),
-						    Form("ADC Values for Plane %hu",plane),
-						    100,0,500);
-      auto& ptr = _hADC_v[plane];
+    auto const plane = img.meta().plane();
+    if (_hADC_v.size() <= plane) _hADC_v.resize(plane + 1, nullptr);
 
-      auto const& adc_v = img.as_vector();
-      for(size_t i=1; i<adc_v.size()-1; ++i) {
-	float pre = adc_v[i-1];
-	float now = adc_v[i];
-	float post = adc_v[i+1];
-	if(pre < now && now > post && now > 5)
-	  ptr->Fill(now);
-      }
+    if (!_hADC_v[plane]) _hADC_v[plane]
+        = new TH1D(Form("hADC_%s_Plane%02hu",
+                        _image_producer.c_str(),
+                        plane), Form("ADC Values for Plane %hu",
+                                     plane), 100, 0, 100);
+    auto& ptr = _hADC_v[plane];
+
+    auto const& adc_v = img.as_vector();
+    for (size_t i = 1; i < adc_v.size() - 1; ++i) {
+      // float pre = adc_v[i - 1];
+      // float now = adc_v[i];
+      // float post = adc_v[i + 1];
+      // if (pre < now && now > post && now > 5)
+      if (adc_v[i] > 0)
+        ptr->Fill(adc_v[i]);
     }
-    return true;
   }
+  return true;
+}
 
-  void ADCScaleAna::finalize()
-  {
-    if(has_ana_file())
+void ADCScaleAna::finalize()
+{
+  if (has_ana_file())
 
-      for(auto& p : _hADC_v) if(p) p->Write();
-  }
+    for (auto& p : _hADC_v) if (p) p->Write();
+}
 
 }
 #endif
