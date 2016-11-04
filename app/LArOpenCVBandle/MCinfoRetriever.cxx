213e71a05a8425d35e1444c80d63775242900e4f
Victor Genty <vgenty@nevis.columbia.edu>
2016-11-04 10:43:09 -0400

    for rui

--- app/LArOpenCVBandle/MCinfoRetriever.cxx
+++ app/LArOpenCVBandle/MCinfoRetriever.cxx
@@ -18,7 +18,17 @@ namespace larcv {
   {}
 
   bool MCinfoRetriever::process(IOManager& mgr)
-  {}
+  {
+
+    // get the ROI data that has the MC information
+    //https://github.com/LArbys/LArCV/blob/develop/core/DataFormat/EventROI.h
+    //https://github.com/LArbys/LArCV/blob/develop/core/DataFormat/ROI.h
+    auto ev_roi = mgr.get_data(kProductROI,"tpc");
+    std::cout << "This event is: " << ev_roi->event() << std::endl;
+
+    return true;
+    
+  }
 
   void MCinfoRetriever::finalize()
   {}
