4f0205dad3f952549493ca707ab39a250aae7ea9
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2017-01-19 20:26:25 -0500

    fixed up. test script runs

--- app/HitVariation/test_hitvariation.py
+++ app/HitVariation/test_hitvariation.py
@@ -4,19 +4,24 @@ from ROOT import std
 from larcv import larcv
 import cv2
 
-#hitvar = larcv.HitVariation()
-hitvar = larcv.HitVariation( larcv_file, sig_translation, sig_amp, sig_width, sig_thresh )
+
+
+hitvar = larcv.HitVariation( "varyhits.cfg" )
+#hitvar = larcv.HitVariation( larcv_file, sig_translation, sig_amp, sig_width, sig_thresh )
+
+# print addres of HitVariation class
+print hitvar
+
 
 #img_v = hivar.genimages(nimgs)
-nimgs = img_v.size()
-for iimg in range(0,nimgs):
-    img2d = img_v.at(iimg)
-    imgnp = larcv.as_ndarray( img2d )
-
-
-    cv2.imwrite(imgname, imgnp)
+#nimgs = img_v.size()
+#for iimg in range(0,nimgs):
+#    img2d = img_v.at(iimg)
+#    imgnp = larcv.as_ndarray( img2d )
+#    imgname=  "test"
+#    cv2.imwrite(imgname, imgnp)
 # draw imgs
 
-print hitvar
+
 
 
