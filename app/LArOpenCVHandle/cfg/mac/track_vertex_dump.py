ef2cf105d5dc2a491fb9ac35da703cf75579b168
Victor Genty <vgenty@nevis.columbia.edu>
2017-02-15 21:24:27 -0500

    update vertex dump

--- app/LArOpenCVHandle/cfg/mac/track_vertex_dump.py
+++ app/LArOpenCVHandle/cfg/mac/track_vertex_dump.py
@@ -1,76 +1,94 @@
 from larcv import larcv
 larcv.load_pyutil
 larcv.load_cvutil
-
 import cv2
 import ROOT
 from ROOT import fcllite
 from ROOT import geo2d
 from ROOT import larocv
-
-
+from ROOT import std
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-proc = larcv.ProcessDriver("ProcessDriver")
-proc.configure('../reco_track_debug.cfg')
-f=ROOT.std.string("/Users/vgenty/Desktop/numu_ccqe_p00_p07.root")
-flist=ROOT.std.vector('string')()
-flist.push_back(f)
+proc = larcv.ProcessDriver('ProcessDriver')
+
+CFG="../reco_track.cfg"
+print "Loading config... ",CFG
+proc.configure(CFG)
+flist=ROOT.std.vector('std::string')()
+flist.push_back("/Users/vgenty/Desktop/numu_8000.root")
 proc.override_input_file(flist)
+filter_id = proc.process_id("NuFilter")
+mcinfo_id = proc.process_id("LArbysImageMC")
+reco_id   = proc.process_id("LArbysImage")
+ana_id    = proc.process_id("LArbysImageAna")
+filter_proc   = proc.process_ptr(filter_id)
+mcinfo_proc   = proc.process_ptr(mcinfo_id)
+mcinfo_proc.SetFilter(filter_proc)
+larbysimg     = proc.process_ptr(reco_id)
+larbysimg_ana = proc.process_ptr(ana_id)
+larbysimg_ana.SetManager(larbysimg.Manager())
+proc.override_ana_file("/tmp/test.root")
 proc.initialize()
-
-filter_id = proc.process_id("NuFilter")                                                                                                             
-mcinfo_id = proc.process_id("LArbysImageMC")                                                                                                        
-reco_id   = proc.process_id("LArbysImage")                                                                                                          
-ana_id    = proc.process_id("LArbysImageAna")                                                                                                       
-                                                                                                                                                    
-filter_proc   = proc.process_ptr(filter_id)                                                                                                         
-mcinfo_proc   = proc.process_ptr(mcinfo_id)                                                                                                         
-mcinfo_proc.SetFilter(filter_proc)                                                                                                                  
-                                                                                                                                                    
-larbysimg     = proc.process_ptr(reco_id)                                                                                                           
-larbysimg_ana = proc.process_ptr(ana_id)                                                                                                            
-larbysimg_ana.SetManager(larbysimg.Manager())   
-
-for event in [247]:
-    print "vic: event is... ",event
+for event in xrange(10):
     proc.batch_process(event,1)
-    continue
-    # if (filter_proc.selected() == False):
-    #     print "Not selected ",event," continue"
-    #     continue
 
-    print "Selected ",event
+    if (filter_proc.selected()==False): continue
+
+    print event
+
     
     mgr=larbysimg.Manager()
     img_v = []
+    track_img_v=[]
+    shower_img_v=[]
     pygeo = geo2d.PyDraw()
 
-    plane=0
-    for mat in mgr.InputImages():
+    for mat in mgr.InputImages(0):
         img_v.append(pygeo.image(mat))
+    for mat in mgr.InputImages(1):
+        track_img_v.append(pygeo.image(mat))
+    for mat in mgr.InputImages(2):
+        shower_img_v.append(pygeo.image(mat))
 
+
+    for plane in xrange(len(track_img_v)):                                                                                                    
+        shower_img=np.where(shower_img_v[plane]>10.0,85.0,0.0).astype(np.uint8)                                                                 
+        track_img=np.where(track_img_v[plane]>10.0,160.0,0.0).astype(np.uint8)                                                                  
+        fig,ax=plt.subplots(figsize=(12,12),facecolor='w')                                                                                      
+        plt.imshow(shower_img+track_img,cmap='jet',interpolation='none',vmin=0.,vmax=255.)                                                      
+        plt.xlabel('Time [6 ticks]',fontsize=20)                                                                                                
+        plt.ylabel('Wire',fontsize=20)                                                                                                          
+        plt.tick_params(labelsize=20)                                                                                                           
+        ax.set_aspect(0.8)                                                                                                                      
+        plt.tight_layout()        
+        SS="out/%04d_00_track_shower_%d.png"%(event,plane)
+        print "Saving ",SS
+        plt.savefig(SS)
+        plt.cla()
+        plt.clf()
+        plt.close()
+
+            
     colors=['red','green','blue','orange','magenta','cyan','pink']
     colors*=10
-
-    dm=mgr.DataManager()
-    data=dm.Data(0,0)
-
+    plane=0
+    print 'Plane',plane
     for plane in xrange(3):
-        ix=0
         fig,ax=plt.subplots(figsize=(12,12),facecolor='w')
-        print 'Plane',plane
-        shape_img = img_v[plane]
-        shape_img=np.where(img_v[plane]>10.0,1.0,0.0).astype(np.uint8)
-        plt.imshow(shape_img,cmap='Greys',interpolation='none')
 
+        shape_img = track_img_v[plane]
+        shape_img=np.where(track_img_v[plane]>0.0,1.0,0.0).astype(np.uint8)
+        plt.imshow(shape_img,cmap='Greys',interpolation='none')
         nz_pixels=np.where(shape_img>0.0)
 
-        vertex_seeds = data.harvest_seeds(plane)
-        print "Plane ",plane," w/ ",vertex_seeds
-        cluscomp_v = vertex_seeds.get_compounds()
+        dm=mgr.DataManager()
+
+        ix=0
+
+        vertex_seeds_v = dm.Data(1,plane).as_vector()
+        cluscomp_v     = dm.Data(1,3+plane).as_vector()
 
         #plot the atomics
         for cluscomp_id in xrange(cluscomp_v.size()):
@@ -78,28 +96,95 @@ for event in [247]:
             for atomic_id in xrange(cluscomp.size()):
                 atomic = cluscomp[atomic_id]
                 pts=[[atomic[p_id].x,atomic[p_id].y] for p_id in xrange(atomic.size())]
-                if len(pts)>0: 
-                    pts.append(pts[0])
-                    pts=np.array(pts)
-                    plt.plot(pts[:,0],pts[:,1],'-o',lw=5,color=colors[ix])
-                    ix+=1
-                    
-        points = data.harvest_seed_points(plane)
-        print "Found number of seeds ",points.size()
-        pts_v = np.array([[points[i].x,points[i].y] for i in xrange(points.size())])
+                pts.append(pts[0])
+                pts=np.array(pts)
+                plt.plot(pts[:,0],pts[:,1],'-o',lw=3,color=colors[ix], alpha = 0.5)
+                ix+=1
+                
+
+        pts_v = np.array([[vertex_seeds_v[i].x,
+                           vertex_seeds_v[i].y] for i in xrange(vertex_seeds_v.size())])
         if pts_v.size>0:
             plt.plot(pts_v[:,0],pts_v[:,1],'*',markersize=20,color='yellow')
 
         ax.set_aspect(1.0)
-        #plt.tight_layout()
-        ax.set_ylim(np.min(nz_pixels[0])-10,np.max(nz_pixels[0])+10)
-        ax.set_xlim(np.min(nz_pixels[1])-10,np.max(nz_pixels[1])+10)
+        plt.tight_layout()
+        try:
+            ax.set_ylim(np.min(nz_pixels[0])-10,np.max(nz_pixels[0])+10)
+            ax.set_xlim(np.min(nz_pixels[1])-10,np.max(nz_pixels[1])+10)
+        except ValueError:
+            pass
+        
         plt.xlabel('Time [6 ticks]',fontsize=20)
         plt.ylabel('Wire [2 wires]',fontsize=20)
         plt.tick_params(labelsize=20)
         ax.set_aspect(0.8)
-        # plt.show()
-        SS="out/%04d_00_seeds_%d.png"%(event,plane)
+        plt.grid()
+        SS="out/%04d_01_atomics_%d.png"%(event,plane)
+        print "Saving ",SS
+        plt.savefig(SS)
+        plt.cla()
+        plt.clf()
+        plt.close()
+
+
+    # In[ ]:
+    tru_vtx_w_v = mcinfo_proc._vtx_2d_w_v;
+    tru_vtx_t_v = mcinfo_proc._vtx_2d_t_v;
+    colors=['red','green','blue','orange','magenta','cyan','pink']
+    colors*=10
+    plane=0
+    print 'Plane',plane
+    for plane in xrange(3):
+        fig,ax=plt.subplots(figsize=(12,12),facecolor='w')
+
+        shape_img = track_img_v[plane]
+        shape_img=np.where(track_img_v[plane]>0.0,1.0,0.0).astype(np.uint8)
+        plt.imshow(shape_img,cmap='Greys',interpolation='none')
+        nz_pixels=np.where(shape_img>0.0)
+
+        dm=mgr.DataManager()
+
+        ix=0
+
+        vertex_vv  = dm.Data(2,0).as_vector()
+        cluscomp_v = dm.Data(1,3+plane).as_vector()
+
+        for cluscomp_id in xrange(cluscomp_v.size()):
+            cluscomp = cluscomp_v[cluscomp_id]
+            for atomic_id in xrange(cluscomp.size()):
+                atomic = cluscomp[atomic_id]
+                pts=[[atomic[p_id].x,atomic[p_id].y] for p_id in xrange(atomic.size())]
+                pts.append(pts[0])
+                pts=np.array(pts)
+                plt.plot(pts[:,0],pts[:,1],'-o',lw=3,color=colors[ix], alpha = 0.5)
+                ix+=1
+
+
+        pts_v = np.array([[vertex_vv[i].cvtx2d_v[plane].center.x,
+                           vertex_vv[i].cvtx2d_v[plane].center.y] for i in xrange(vertex_vv.size())])
+        if pts_v.size>0:
+            print "Vertex Candidates @\n",pts_v
+            plt.plot(pts_v[:,0],pts_v[:,1],'*',markersize=30,color='cyan')
+
+
+        tru_vtx_w = tru_vtx_w_v[plane]
+        tru_vtx_t = tru_vtx_t_v[plane]
+
+        plt.plot(tru_vtx_t,tru_vtx_w,marker='*',markersize=35,color='yellow',alpha=0.5)
+        
+        ax.set_aspect(1.0)
+        plt.tight_layout()
+        try:
+            ax.set_ylim(np.min(nz_pixels[0])-10,np.max(nz_pixels[0])+10)
+            ax.set_xlim(np.min(nz_pixels[1])-10,np.max(nz_pixels[1])+10)
+        except ValueError:
+            pass
+        plt.xlabel('Time [6 ticks]',fontsize=20)
+        plt.ylabel('Wire [2 wires]',fontsize=20)
+        plt.tick_params(labelsize=20)
+        ax.set_aspect(0.8)
+        SS="out/%04d_02_vertex_%d.png"%(event,plane)
         print "Saving ",SS
         plt.savefig(SS)
         plt.cla()
@@ -110,10 +195,8 @@ for event in [247]:
     # In[ ]:
 
     import matplotlib.patches as patches
-    alg=mgr.GetClusterAlg(1).Algo()
+    alg=mgr.GetClusterAlg(2).Algo()
 
-    dm=mgr.DataManager()
-    ref_data = dm.Data(1)
     tickscore0_y=[]
     tickscore0_x=[]
 
@@ -164,8 +247,7 @@ for event in [247]:
     ax.set_xlim(tickscore0_x.min(),tickscore0_x.max())
     plt.grid()
     #ax.set_xlim(450,480)
-    #plt.show()
-    SS="out/%04d_01_tickscore.png"%(event)
+    SS="out/%04d_03_score_%d.png"%(event,plane)
     print "Saving ",SS
     plt.savefig(SS)
     plt.cla()
@@ -173,139 +255,46 @@ for event in [247]:
     plt.close()
 
 
-    import matplotlib.patches as patches
-    alg=mgr.GetClusterAlg(1).Algo()
+    # assman=dm.AssManager()
+    # #New VertexCluster
+    # vtx_data=dm.Data(2,0).as_vector()
 
-    dm=mgr.DataManager()
-    ref_data = dm.Data(1)
-    for plane in xrange(3):
-        wirescore_y = []
-        wirescore_x = []
-        score_v = alg.WireBinnedScoreMean(plane)
-        for idx in xrange(score_v.size()):
-            wirescore_y.append(score_v[idx])
-            wirescore_x.append(idx + alg.WireBinMin(plane))
+    # #Simply a list of VertexCluster (i.e. vertex-wise list of clusters)
+    # print "There are ",vtx_data.size(),"vertex found in this event. Note that this is vertex-wise"
+    # ix=-1
+    # for vtx in vtx_data:
+    #     ix+=1
+    #     print "<===================Start Vertex3D number ",ix," ==========================>"
+    #     for plane in xrange(3):
+    #         fig,ax = plt.subplots(figsize=(12,12),facecolor='w')
+    #         shape_img = img_v[plane]
+    #         shape_img=np.where(img_v[plane]>10.0,1.0,0.0).astype(np.uint8)
+    #         plt.imshow(shape_img,cmap='Greys',interpolation='none')
+    #         nz_pixels=np.where(shape_img>0.0)
 
-        wirescore_y = np.array(wirescore_y)
-        wirescore_x = np.array(wirescore_x)
+    #         par_data=dm.Data(4,plane)
 
+    #         ass_t = np.array(assman.GetManyAss(vtx,par_data.ID()))
+    #         if ass_t.size==0:continue
 
-        xmin=0
-        xmax=1
-        ymin=0
-        ymax=1
+    #         par_data_v=par_data.as_vector()
+    #         for id_ in ass_t:
+    #             ctor=np.array([[pt.x,pt.y] for pt in par_data_v[id_]._ctor])
+    #             plt.plot(ctor[:,0],ctor[:,1],'-o',lw=2)
 
-        if wirescore_y.size > 0 :
-            ymin = wirescore_y.min()
-            ymax = wirescore_y.max()
+    #         ax.set_aspect(1.0)
+    #         plt.tight_layout()
+    #         ax.set_ylim(np.min(nz_pixels[0])-10,np.max(nz_pixels[0])+10)
+    #         ax.set_xlim(np.min(nz_pixels[1])-10,np.max(nz_pixels[1])+10)
+    #         plt.xlabel('Time [6 ticks]',fontsize=20)
+    #         plt.ylabel('Wire [2 wires]',fontsize=20)
+    #         plt.tick_params(labelsize=20)
+    #         plt.grid()
+    #         SS="out/%04d_04_particle_%d_%d.png"%(event,ix,plane)
+    #         print "Saving ",SS
+    #         plt.savefig(SS)
+    #         plt.cla()
+    #         plt.clf()
+    #         plt.close()
 
-        if wirescore_x.size > 0 :
-            xmin = wirescore_x.min()
-            xmax = wirescore_x.max()
-
-        fig,ax = plt.subplots(figsize=(16,8),facecolor='w')
-        plt.plot(wirescore_x,wirescore_y,marker='o',linestyle='-',color='blue',markersize=10)
-
-
-        minimum_v  = alg.WireBinnedScoreMinIndex(plane)
-        minrange_v = alg.WireBinnedScoreMinRange(plane)
-        for idx in xrange(minimum_v.size()):
-            xval = wirescore_x[minimum_v[idx]]
-            plt.plot([xval,xval],[0,360],marker='',linestyle='--',color='black',linewidth=2)
-            xstart = wirescore_x[minrange_v[idx].first]
-            xend   = wirescore_x[minrange_v[idx].second]
-            ax.axvspan(xstart,xend, alpha=0.3, color='orange')
-
-        plt.xlabel('Wire [2 wires]',fontsize=20,fontweight='bold')
-        plt.ylabel('Summed Angle Difference [deg.]',fontsize=20,fontweight='bold')
-        plt.tick_params(labelsize=20)
-        plt.tight_layout()
-        ax=plt.gca()
-        ax.set_ylim(-1,ymax*1.1)
-        ax.set_xlim(xmin,xmax)
-        plt.grid()
-        #plt.show()
-        SS="out/%04d_02_wirescore.png"%(event)
-        print "Saving ",SS
-        plt.savefig(SS)
-        plt.cla()
-        plt.clf()
-        plt.close()
-
-
-
-    # In[ ]:
-
-    import matplotlib.patches as patches
-
-    tru_vtx_w_v = mcinfo_proc._vtx_2d_w_v;
-    tru_vtx_t_v = mcinfo_proc._vtx_2d_t_v;
-    
-    for plane in xrange(3):
-        fig,ax=plt.subplots(figsize=(12,12),facecolor='w')
-        shape_img=np.where(img_v[plane]>10.0,1.0,0.0).astype(np.uint8)
-        plt.imshow(shape_img,cmap='Greys',interpolation='none')
-        nz_pixels=np.where(shape_img>0.0)
-
-        alg = mgr.GetClusterAlg(1).Algo()
-
-        cv=larbysimg.Manager().GetClusterAlg(1)
-        mgr=larbysimg.Manager()
-        dm=mgr.DataManager()
-
-        #Getting seeds
-        seeds_data=dm.Data(0)
-        seeds = seeds_data.harvest_seeds(plane)
-        for compound in seeds.get_compounds():
-            for defect in compound.get_defects():
-                pt = defect._pt_defect
-                plt.plot([pt.x],[pt.y],marker='o',color='blue',markersize=10)
-        for pt in seeds.get_pcaxs():
-            plt.plot([pt.x],[pt.y],marker='o',color='red',markersize=10)
-
-        ref_data   = dm.Data(1)
-        plane_scan = alg.PlaneInfo(plane)
-
-        for circle in plane_scan._circle_scan_v:
-            c=patches.Circle((circle.center.x,circle.center.y),circle.radius,ec='cyan',alpha=0.05,fc='none',lw=10)
-            ax.add_patch(c)
-
-        minimum_v = alg.TimeBinnedScoreMinIndex()
-        for idx in xrange(minimum_v.size()):
-            xval = minimum_v[idx] + alg.TimeBinMin()
-            plt.plot([xval,xval],[0,2000],marker='',linestyle='--',color='magenta',linewidth=2)
-
-        vtx_vv = ref_data.get_circle_vertex()
-        print "Got ",vtx_vv.size(), " circle vertex"
-        for vtx_idx in xrange(len(vtx_vv)):
-            vtx = vtx_vv[vtx_idx][plane]
-            color='magenta'
-            if ref_data.get_type(vtx_idx) == 1:
-                color='yellow'
-            plt.plot([vtx.center.x],[vtx.center.y],marker='$\star$',color=color,markersize=24)
-
-        ymin,ymax = (np.min(nz_pixels[0])-10,np.max(nz_pixels[0])+10)
-        xmin,xmax = (np.min(nz_pixels[1])-10,np.max(nz_pixels[1])+10)
-
-        ax.set_ylim(ymin,ymax)
-        ax.set_xlim(xmin,xmax)
-        plt.xlabel('Time [6 ticks]',fontsize=20)
-        plt.ylabel('Wire [2 wires]',fontsize=20)
-        plt.tick_params(labelsize=20)
-        ax.set_aspect(0.8)
-        plt.tight_layout()
-        ax=plt.gca()
-
-        tru_vtx_w = tru_vtx_w_v[plane]
-        tru_vtx_t = tru_vtx_t_v[plane]
-
-        plt.plot(tru_vtx_t,tru_vtx_w,marker='*',markersize=35,color='yellow',alpha=0.5)
-        
-        SS="out/%04d_04_vertex_%d.png"%(event,plane)
-        print SS
-        plt.savefig(SS)
-        plt.cla()
-        plt.clf()
-        plt.close()
-
-
+    #     print "<===================End   Vertex3D number ",ix," ==========================>"
