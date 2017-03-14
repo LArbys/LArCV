b332f102f3821b8748bfc16bf0b4af9c12e0139e
Victor Genty <vgenty@nevis.columbia.edu>
2017-03-14 16:32:34 -0400

    update Result

--- app/LArOpenCVHandle/LArbysImageExtract.cxx
+++ app/LArOpenCVHandle/LArbysImageExtract.cxx
@@ -26,7 +26,11 @@ namespace larcv {
   bool LArbysImageExtract::process(IOManager& mgr)
   {
 
-    auto adc_img_data_v = _LArbysImageMaker.ExtractImage(mgr,_adc_producer);
+    auto ev_adc = (EventImage2D*)mgr.get_data(kProductImage2D,_adc_producer);
+    auto ev_trk = (EventImage2D*)mgr.get_data(kProductImage2D,_track_producer);
+    auto ev_shr = (EventImage2D*)mgr.get_data(kProductImage2D,_shower_producer);
+    
+    auto adc_img_data_v = _LArbysImageMaker.ExtractImage(ev_adc->Image2DArray());
     _adc_mat_v.clear();
     _adc_mat_v.reserve(3);
     
@@ -38,8 +42,8 @@ namespace larcv {
       _adc_meta_v.emplace_back(std::move(std::get<1>(img_data)));
     }
     
-    _track_mat_v = _LArbysImageMaker.ExtractMat(mgr,_track_producer);
-    _shower_mat_v = _LArbysImageMaker.ExtractMat(mgr,_shower_producer);
+    _track_mat_v = _LArbysImageMaker.ExtractMat(ev_trk->Image2DArray());
+    _shower_mat_v = _LArbysImageMaker.ExtractMat(ev_shr->Image2DArray());
 
     return true;
   }
