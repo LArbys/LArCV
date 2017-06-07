176fc6aefb5c4c1b2dd0bffedd907ee221aaf9b3
drinkingkazu <kazuhiro@nevis.columbia.edu>
2017-06-07 14:25:34 -0400

    fucking hell

--- configure.sh
+++ configure.sh
@@ -47,9 +47,14 @@ fi
 export LARCV_NUMPY=`$LARCV_BASEDIR/bin/check_numpy`
 
 # Set ANN directories
-export LARCV_ANN=1
-export ANN_INCDIR=$LARCV_BASEDIR/app/ann_1.1.2/include
-export ANN_LIBDIR=$LARCV_BASEDIR/app/ann_1.1.2/lib
+if [[ -z $LARCV_ANN ]]; then
+    export LARCV_ANN=1
+fi
+
+if [ $LARCV_ANN -eq 1 ]; then
+    export ANN_INCDIR=$LARCV_BASEDIR/app/ann_1.1.2/include
+    export ANN_LIBDIR=$LARCV_BASEDIR/app/ann_1.1.2/lib
+fi
 
 # warning for missing support
 missing=""
