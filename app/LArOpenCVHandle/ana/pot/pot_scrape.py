e7db825ca00cd56fa1910267424bd148a3414030
Victor Genty <vgenty@nevis.columbia.edu>
2017-09-11 12:33:10 -0400

    update pot script

--- app/LArOpenCVHandle/ana/pot/pot_scrape.py
+++ app/LArOpenCVHandle/ana/pot/pot_scrape.py
@@ -3,17 +3,39 @@ import ROOT
 import numpy as np
 import root_numpy as rn
 import pandas as pd
+from array import array
 
 FILE=str(sys.argv[1])
-NAME=str(os.path.basename(FILE))
+print "GOT %s"%FILE
+NAME=str(os.path.basename(FILE).split(".")[0])
+print "NAME %s"%NAME
 df = pd.DataFrame(rn.root2array(FILE,treename="analysistree/pottree"))[['run','subrun','pot']]
-df['pot_fname'] = np.array([ROOT.std.string(NAME) for _ in xrange(df.index.size)])
-rec = df.to_records()
+df['pot_fname'] = NAME
 FOUT="pot_%s.root"%NAME
 tf = ROOT.TFile.Open(FOUT,"RECREATE")
 print "OPEN %s"%FOUT
 tf.cd()
-tree = rn.array2tree(rec)
+
+run    = array( 'i', [ 0 ] )
+subrun = array( 'i', [ 0 ] )
+pot    = array( 'd', [ 0 ] )
+fname  = ROOT.std.string()
+
+tree = ROOT.TTree("pot_tree","")
+
+tree.Branch("run"      , run   , "run/I")
+tree.Branch("subrun"   , subrun, "subrun/I")
+tree.Branch("pot"      , pot   , "pot/D")
+tree.Branch("pot_fname", fname)
+
+for index,row in df.iterrows():
+    run[0]    = int(row.run)
+    subrun[0] = int(row.subrun)
+    pot[0]    = long(row.pot)
+    fname.replace(0,ROOT.std.string.npos,ROOT.std.string(row.pot_fname))
+
+    tree.Fill()
+
 tree.Write()
 print "WRITE %s"%tree.GetName()
 tf.Close()
