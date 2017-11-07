fc1071a1a0eb3e08dd2bf961bf47e06d69e03696
Victor Genty <vgenty@nevis.columbia.edu>
2017-11-07 11:20:03 -0500

    project track onto image

--- app/Reco3D/AStarUtils.cxx
+++ app/Reco3D/AStarUtils.cxx
@@ -5,7 +5,7 @@
 #include "LArUtil/GeometryHelper.h"
 #include "LArUtil/LArProperties.h"
 #include <numeric>
-#include <assert.h>
+#include <cassert>
 
 namespace larcv {
 
@@ -30,6 +30,41 @@ namespace larcv {
     ypixel = (((parent_x/larpro->DriftVelocity() + parent_t/1000.)*2+3200)-meta.br().y)/y_compression;
   }  
   
+  std::vector<std::vector<cv::Point_<int> > > TrackToPixels(const std::vector<TVector3>& xyz_v,
+							      const std::vector<ImageMeta>& meta_v) {
+
+    std::vector<std::vector<cv::Point_<int> > > res_vv;
+    res_vv.resize(3);
+    assert(res_vv.size() == meta_v.size());
+
+    for(auto& res_v : res_vv) res_v.resize(xyz_v.size());
+
+    double xpixel = kINVALID_DOUBLE;
+    double ypixel = kINVALID_DOUBLE;
+
+    for(size_t tid=0; tid<xyz_v.size(); ++ tid) {
+      const auto& trk_pt = xyz_v[tid];
+      for(size_t plane=0; plane<3; ++plane) {
+
+	xpixel = kINVALID_DOUBLE;
+	ypixel = kINVALID_DOUBLE;
+
+	auto& res_v = res_vv[plane];
+	const auto& meta = meta_v[plane];
+	ProjectTo3D(meta, trk_pt.X(), trk_pt.Y(), trk_pt.Z(), 0.0, plane, xpixel, ypixel);
+	
+	int xx = (int)(xpixel+0.5);
+	int yy = (int)(ypixel+0.5);
+	yy = meta.rows() - yy - 1;
+	
+	res_v[tid] = cv::Point(yy,xx);
+      }
+    }    
+
+    return res_vv;
+  }
+
+
 }
 
 #endif
