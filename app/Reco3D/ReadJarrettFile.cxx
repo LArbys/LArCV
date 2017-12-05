e7a249647cd3ada7c277a1f880ea64c38b8c723e
jmoon2610 <jarrett@mit.edu>
2017-12-05 14:33:53 -0600

    Fix SC offsetts

--- app/Reco3D/ReadJarrettFile.cxx
+++ app/Reco3D/ReadJarrettFile.cxx
@@ -315,8 +315,8 @@ namespace larcv {
 	  auto const sceOffset = sce.GetPosOffsets(pt_X,pt_Y,pt_Z);
 
 	  double sceptX = pt_X - sceOffset[0] + 0.7;
-	  double sceptY = pt_Y - sceOffset[1];
-	  double sceptZ = pt_Z - sceOffset[2];
+	  double sceptY = pt_Y + sceOffset[1];
+	  double sceptZ = pt_Z + sceOffset[2];
 	  
             //if(_isMC && !IsGoodVertex(run,subrun,event,pgraph_id)){continue;}
             //if(!IsGoodVertex(run,subrun,event,pgraph_id)){ continue;}
