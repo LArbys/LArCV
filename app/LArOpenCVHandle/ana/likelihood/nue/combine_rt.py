4b4abce07641e13cf2dff372cac4226413fea880
Victor Genty <vgenty@nevis.columbia.edu>
2018-02-07 13:40:52 -0500

    update combined_rt

--- app/LArOpenCVHandle/ana/likelihood/nue/combine_rt.py
+++ app/LArOpenCVHandle/ana/likelihood/nue/combine_rt.py
@@ -1,10 +1,11 @@
 import os, sys, gc
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     print 
     print "VERTEX_PKL   = str(sys.argv[1]) -- ana_comb_df"
     print "NUMU_LL_ROOT = str(sys.argv[2]) -- FinalVertexVariables"
-    print "OUTDIR       = str(sys.argv[3])"
+    print "IS_MC        = str(sys.argv[3]) -- IS_MC"
+    print "OUTDIR       = str(sys.argv[4])"
     print 
     sys.exit(1)
 
@@ -17,16 +18,19 @@ sys.path.insert(0,BASE_PATH)
 
 VERTEX_PKL   = str(sys.argv[1])
 NUMU_LL_ROOT = str(sys.argv[2])
-OUTDIR       = str(sys.argv[3])
+IS_MC        = bool(int(sys.argv[3]))
+OUTDIR       = str(sys.argv[4])
 
 num = int(os.path.basename(VERTEX_PKL).split(".")[0].split("_")[-1])
 
 from util.fill_df import *
+from util.ll_functions import *
 
 print "--> initialize_rt(...)"
 
 rt_df = initialize_rt(VERTEX_PKL,NUMU_LL_ROOT)
-                      
+rt_df = prep_common_vars(rt_df,IS_MC)                      
+
 rt_df.to_pickle(os.path.join(OUTDIR,"rt_LL_comb_df_%d.pkl" % num))
 del rt_df
 gc.collect()
