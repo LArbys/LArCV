e6b2bfd416f46552289e82d14051ccdbb013c344
Victor Genty <vgenty@nevis.columbia.edu>
2018-02-28 17:02:07 -0500

    sigh

--- app/LArOpenCVHandle/ana/final_file/lib/nue_handler.py
+++ app/LArOpenCVHandle/ana/final_file/lib/nue_handler.py
@@ -179,11 +179,11 @@ class NueHandler(Handler):
         self.rd.reco_proton_phi[0]   = float(row['reco_LL_proton_phi'])
         self.rd.reco_proton_dEdx[0]  = float(row['reco_LL_proton_dEdx'])
         self.rd.reco_proton_length[0]= float(row['reco_LL_proton_length'])
-        self.rd.reco_proton_mean_pixel_dist[0] = float(row['reco_proton_mean_pixel_dist'])
-        self.rd.reco_proton_width[0]       = float(row['reco_proton_width'])
-        self.rd.reco_proton_area[0]        = float(row['reco_proton_area'])
-        self.rd.reco_proton_qsum[0]        = float(row['reco_proton_qsum'])
-        self.rd.reco_proton_shower_frac[0] = float(row['reco_proton_shower_frac'])
+        self.rd.reco_proton_mean_pixel_dist[0] = float(row['reco_LL_proton_mean_pixel_dist'])
+        self.rd.reco_proton_width[0]       = float(row['reco_LL_proton_width'])
+        self.rd.reco_proton_area[0]        = float(row['reco_LL_proton_area'])
+        self.rd.reco_proton_qsum[0]        = float(row['reco_LL_proton_qsum'])
+        self.rd.reco_proton_shower_frac[0] = float(row['reco_LL_proton_shower_frac'])
 
         self.rd.reco_electron_id[0]     = int(row['reco_LL_electron_id'])
         self.rd.reco_electron_E[0]      = float(row['reco_LL_electron_energy']);
@@ -191,11 +191,11 @@ class NueHandler(Handler):
         self.rd.reco_electron_phi[0]    = float(row['reco_LL_electron_phi'])
         self.rd.reco_electron_dEdx[0]   = float(row['reco_LL_electron_dEdx'])
         self.rd.reco_electron_length[0] = float(row['reco_LL_electron_length'])
-        self.rd.reco_electron_mean_pixel_dist[0] = float(row['reco_electron_mean_pixel_dist'])
-        self.rd.reco_electron_width[0]       = float(row['reco_electron_width'])
-        self.rd.reco_electron_area[0]        = float(row['reco_electron_area'])
-        self.rd.reco_electron_qsum[0]        = float(row['reco_electron_qsum'])
-        self.rd.reco_electron_shower_frac[0] = float(row['reco_electron_shower_frac'])
+        self.rd.reco_electron_mean_pixel_dist[0] = float(row['reco_LL_electron_mean_pixel_dist'])
+        self.rd.reco_electron_width[0]       = float(row['reco_LL_electron_width'])
+        self.rd.reco_electron_area[0]        = float(row['reco_LL_electron_area'])
+        self.rd.reco_electron_qsum[0]        = float(row['reco_LL_electron_qsum'])
+        self.rd.reco_electron_shower_frac[0] = float(row['reco_LL_electron_shower_frac'])
 
         self.rd.reco_total_E[0]    = float(row['reco_LL_total_energy']);
 
