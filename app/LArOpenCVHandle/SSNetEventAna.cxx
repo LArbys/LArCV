03660cf40f87000b3b1ad824e41a2807bd7b3909
Victor Genty <vgenty@nevis.columbia.edu>
2018-03-19 11:55:33 -0400

    update

--- app/LArOpenCVHandle/SSNetEventAna.cxx
+++ app/LArOpenCVHandle/SSNetEventAna.cxx
@@ -41,65 +41,65 @@ namespace larcv {
     _tree->Branch("n_trk_pixel1",&_n_trk_pixel1,"_n_trk_pixel1/F");
     _tree->Branch("n_trk_pixel2",&_n_trk_pixel2,"_n_trk_pixel2/F");
 
-    _tree->Branch("avg_trk_pixel_val0", &_avg_trk_pixel_val0, "_avg_trk_pixel_val0/F");
-    _tree->Branch("avg_trk_pixel_val1", &_avg_trk_pixel_val1, "_avg_trk_pixel_val1/F");
-    _tree->Branch("avg_trk_pixel_val2", &_avg_trk_pixel_val2, "_avg_trk_pixel_val2/F");
+    _tree->Branch("avg_trk_pixel_val0", &_avg_trk_pixel_val0, "avg_trk_pixel_val0/F");
+    _tree->Branch("avg_trk_pixel_val1", &_avg_trk_pixel_val1, "avg_trk_pixel_val1/F");
+    _tree->Branch("avg_trk_pixel_val2", &_avg_trk_pixel_val2, "avg_trk_pixel_val2/F");
 
-    _tree->Branch("std_trk_pixel_val0", &_std_trk_pixel_val0, "_std_trk_pixel_val0/F");
-    _tree->Branch("std_trk_pixel_val1", &_std_trk_pixel_val1, "_std_trk_pixel_val1/F");
-    _tree->Branch("std_trk_pixel_val2", &_std_trk_pixel_val2, "_std_trk_pixel_val2/F");
+    _tree->Branch("std_trk_pixel_val0", &_std_trk_pixel_val0, "std_trk_pixel_val0/F");
+    _tree->Branch("std_trk_pixel_val1", &_std_trk_pixel_val1, "std_trk_pixel_val1/F");
+    _tree->Branch("std_trk_pixel_val2", &_std_trk_pixel_val2, "std_trk_pixel_val2/F");
 
-    _tree->Branch("tot_trk_pixel_val0", &_tot_trk_pixel_val0, "_tot_trk_pixel_val0/F");
-    _tree->Branch("tot_trk_pixel_val1", &_tot_trk_pixel_val1, "_tot_trk_pixel_val1/F");
-    _tree->Branch("tot_trk_pixel_val2", &_tot_trk_pixel_val2, "_tot_trk_pixel_val2/F");
+    _tree->Branch("tot_trk_pixel_val0", &_tot_trk_pixel_val0, "tot_trk_pixel_val0/F");
+    _tree->Branch("tot_trk_pixel_val1", &_tot_trk_pixel_val1, "tot_trk_pixel_val1/F");
+    _tree->Branch("tot_trk_pixel_val2", &_tot_trk_pixel_val2, "tot_trk_pixel_val2/F");
 
-    _tree->Branch("avg_trk_score0", &_avg_trk_score0, "_avg_trk_score0/F");
-    _tree->Branch("avg_trk_score1", &_avg_trk_score1, "_avg_trk_score1/F");
-    _tree->Branch("avg_trk_score2", &_avg_trk_score2, "_avg_trk_score2/F");
+    _tree->Branch("avg_trk_score0", &_avg_trk_score0, "avg_trk_score0/F");
+    _tree->Branch("avg_trk_score1", &_avg_trk_score1, "avg_trk_score1/F");
+    _tree->Branch("avg_trk_score2", &_avg_trk_score2, "avg_trk_score2/F");
 
-    _tree->Branch("std_trk_score0", &_std_trk_score0, "_std_trk_score0/F");
-    _tree->Branch("std_trk_score1", &_std_trk_score1, "_std_trk_score1/F");
-    _tree->Branch("std_trk_score2", &_std_trk_score2, "_std_trk_score2/F");
+    _tree->Branch("std_trk_score0", &_std_trk_score0, "std_trk_score0/F");
+    _tree->Branch("std_trk_score1", &_std_trk_score1, "std_trk_score1/F");
+    _tree->Branch("std_trk_score2", &_std_trk_score2, "std_trk_score2/F");
 
-    _tree->Branch("n_shr_pixel0",&_n_shr_pixel0,"_n_shr_pixel0/F");
-    _tree->Branch("n_shr_pixel1",&_n_shr_pixel1,"_n_shr_pixel1/F");
-    _tree->Branch("n_shr_pixel2",&_n_shr_pixel2,"_n_shr_pixel2/F");
+    _tree->Branch("n_shr_pixel0",&_n_shr_pixel0,"n_shr_pixel0/F");
+    _tree->Branch("n_shr_pixel1",&_n_shr_pixel1,"n_shr_pixel1/F");
+    _tree->Branch("n_shr_pixel2",&_n_shr_pixel2,"n_shr_pixel2/F");
 
-    _tree->Branch("avg_shr_pixel_val0", &_avg_shr_pixel_val0, "_avg_shr_pixel_val0/F");
-    _tree->Branch("avg_shr_pixel_val1", &_avg_shr_pixel_val1, "_avg_shr_pixel_val1/F");
-    _tree->Branch("avg_shr_pixel_val2", &_avg_shr_pixel_val2, "_avg_shr_pixel_val2/F");
+    _tree->Branch("avg_shr_pixel_val0", &_avg_shr_pixel_val0, "avg_shr_pixel_val0/F");
+    _tree->Branch("avg_shr_pixel_val1", &_avg_shr_pixel_val1, "avg_shr_pixel_val1/F");
+    _tree->Branch("avg_shr_pixel_val2", &_avg_shr_pixel_val2, "avg_shr_pixel_val2/F");
 
-    _tree->Branch("std_shr_pixel_val0", &_std_shr_pixel_val0, "_std_shr_pixel_val0/F");
-    _tree->Branch("std_shr_pixel_val1", &_std_shr_pixel_val1, "_std_shr_pixel_val1/F");
-    _tree->Branch("std_shr_pixel_val2", &_std_shr_pixel_val2, "_std_shr_pixel_val2/F");
+    _tree->Branch("std_shr_pixel_val0", &_std_shr_pixel_val0, "std_shr_pixel_val0/F");
+    _tree->Branch("std_shr_pixel_val1", &_std_shr_pixel_val1, "std_shr_pixel_val1/F");
+    _tree->Branch("std_shr_pixel_val2", &_std_shr_pixel_val2, "std_shr_pixel_val2/F");
 
-    _tree->Branch("tot_shr_pixel_val0", &_tot_shr_pixel_val0, "_tot_shr_pixel_val0/F");
-    _tree->Branch("tot_shr_pixel_val1", &_tot_shr_pixel_val1, "_tot_shr_pixel_val1/F");
-    _tree->Branch("tot_shr_pixel_val2", &_tot_shr_pixel_val2, "_tot_shr_pixel_val2/F");
+    _tree->Branch("tot_shr_pixel_val0", &_tot_shr_pixel_val0, "tot_shr_pixel_val0/F");
+    _tree->Branch("tot_shr_pixel_val1", &_tot_shr_pixel_val1, "tot_shr_pixel_val1/F");
+    _tree->Branch("tot_shr_pixel_val2", &_tot_shr_pixel_val2, "tot_shr_pixel_val2/F");
 
-    _tree->Branch("avg_shr_score0", &_avg_shr_score0, "_avg_shr_score0/F");
-    _tree->Branch("avg_shr_score1", &_avg_shr_score1, "_avg_shr_score1/F");
-    _tree->Branch("avg_shr_score2", &_avg_shr_score2, "_avg_shr_score2/F");
+    _tree->Branch("avg_shr_score0", &_avg_shr_score0, "avg_shr_score0/F");
+    _tree->Branch("avg_shr_score1", &_avg_shr_score1, "avg_shr_score1/F");
+    _tree->Branch("avg_shr_score2", &_avg_shr_score2, "avg_shr_score2/F");
 
-    _tree->Branch("std_shr_score0", &_std_shr_score0, "_std_shr_score0/F");
-    _tree->Branch("std_shr_score1", &_std_shr_score1, "_std_shr_score1/F");
-    _tree->Branch("std_shr_score2", &_std_shr_score2, "_std_shr_score2/F");
+    _tree->Branch("std_shr_score0", &_std_shr_score0, "std_shr_score0/F");
+    _tree->Branch("std_shr_score1", &_std_shr_score1, "std_shr_score1/F");
+    _tree->Branch("std_shr_score2", &_std_shr_score2, "std_shr_score2/F");
 
-    _tree->Branch("n_adcpixel0",&_n_adc_pixel0,"_n_adc_pixel0/F");
-    _tree->Branch("n_adcpixel1",&_n_adc_pixel1,"_n_adc_pixel1/F");
-    _tree->Branch("n_adcpixel2",&_n_adc_pixel2,"_n_adc_pixel2/F");
+    _tree->Branch("n_adcpixel0",&_n_adc_pixel0,"n_adc_pixel0/F");
+    _tree->Branch("n_adcpixel1",&_n_adc_pixel1,"n_adc_pixel1/F");
+    _tree->Branch("n_adcpixel2",&_n_adc_pixel2,"n_adc_pixel2/F");
 
-    _tree->Branch("avg_adc_pixel_val0", &_avg_adc_pixel_val0, "_avg_adc_pixel_val0/F");
-    _tree->Branch("avg_adc_pixel_val1", &_avg_adc_pixel_val1, "_avg_adc_pixel_val1/F");
-    _tree->Branch("avg_adc_pixel_val2", &_avg_adc_pixel_val2, "_avg_adc_pixel_val2/F");
+    _tree->Branch("avg_adc_pixel_val0", &_avg_adc_pixel_val0, "avg_adc_pixel_val0/F");
+    _tree->Branch("avg_adc_pixel_val1", &_avg_adc_pixel_val1, "avg_adc_pixel_val1/F");
+    _tree->Branch("avg_adc_pixel_val2", &_avg_adc_pixel_val2, "avg_adc_pixel_val2/F");
 
-    _tree->Branch("std_adc_pixel_val0", &_std_adc_pixel_val0, "_std_adc_pixel_val0/F");
-    _tree->Branch("std_adc_pixel_val1", &_std_adc_pixel_val1, "_std_adc_pixel_val1/F");
-    _tree->Branch("std_adc_pixel_val2", &_std_adc_pixel_val2, "_std_adc_pixel_val2/F");
+    _tree->Branch("std_adc_pixel_val0", &_std_adc_pixel_val0, "std_adc_pixel_val0/F");
+    _tree->Branch("std_adc_pixel_val1", &_std_adc_pixel_val1, "std_adc_pixel_val1/F");
+    _tree->Branch("std_adc_pixel_val2", &_std_adc_pixel_val2, "std_adc_pixel_val2/F");
 
-    _tree->Branch("tot_adc_pixel_val0", &_tot_adc_pixel_val0, "_tot_adc_pixel_val0/F");
-    _tree->Branch("tot_adc_pixel_val1", &_tot_adc_pixel_val1, "_tot_adc_pixel_val1/F");
-    _tree->Branch("tot_adc_pixel_val2", &_tot_adc_pixel_val2, "_tot_adc_pixel_val2/F");
+    _tree->Branch("tot_adc_pixel_val0", &_tot_adc_pixel_val0, "tot_adc_pixel_val0/F");
+    _tree->Branch("tot_adc_pixel_val1", &_tot_adc_pixel_val1, "tot_adc_pixel_val1/F");
+    _tree->Branch("tot_adc_pixel_val2", &_tot_adc_pixel_val2, "tot_adc_pixel_val2/F");
 
     Reset();
   }
@@ -149,7 +149,27 @@ namespace larcv {
     std::array<float,3> avg_adc_pixel_val_v;
     std::array<float,3> std_adc_pixel_val_v;
     std::array<float,3> tot_adc_pixel_val_v;
-    
+
+    for(size_t plane=0; plane<3; ++plane) {
+      n_trk_pixel_v[plane] = 0;
+      avg_trk_pixel_val_v[plane] = 0;
+      std_trk_pixel_val_v[plane] = 0;
+      tot_trk_pixel_val_v[plane] = 0;
+      avg_trk_score_v[plane] = 0;
+      std_trk_score_v[plane] = 0;
+
+      n_shr_pixel_v[plane] = 0;
+      avg_shr_pixel_val_v[plane] = 0;
+      std_shr_pixel_val_v[plane] = 0;
+      tot_shr_pixel_val_v[plane] = 0;
+      avg_shr_score_v[plane] = 0;
+      std_shr_score_v[plane] = 0;
+
+      n_adc_pixel_v[plane] = 0;
+      avg_adc_pixel_val_v[plane] = 0;
+      std_adc_pixel_val_v[plane] = 0;
+      tot_adc_pixel_val_v[plane] = 0;
+    }
     
     for(size_t plane=0; plane<3; ++plane) {
       const auto& adc_img   = ev_img2d->Image2DArray()[plane];
@@ -165,16 +185,16 @@ namespace larcv {
 
       switch(plane) {
       case 0:  
-	trk_score_img_ptr = &ev_score0->Image2DArray().back(); 
-	shr_score_img_ptr = &ev_score0->Image2DArray().front(); 
+	trk_score_img_ptr = &(ev_score0->Image2DArray().back()); 
+	shr_score_img_ptr = &(ev_score0->Image2DArray().front()); 
 	break;
       case 1:  
-	trk_score_img_ptr = &ev_score1->Image2DArray().back();
-	shr_score_img_ptr = &ev_score1->Image2DArray().front();
+	trk_score_img_ptr = &(ev_score1->Image2DArray().back());
+        shr_score_img_ptr = &(ev_score1->Image2DArray().front());
 	break;
       case 2:  
-	trk_score_img_ptr = &ev_score2->Image2DArray().back();
-	shr_score_img_ptr = &ev_score2->Image2DArray().front();
+        trk_score_img_ptr = &(ev_score2->Image2DArray().back());
+        shr_score_img_ptr = &(ev_score2->Image2DArray().front());
 	break;
       default: 
 	throw larbys("die");
@@ -182,6 +202,11 @@ namespace larcv {
 
       const auto& trk_score_vec = trk_score_img_ptr->as_vector();
       const auto& shr_score_vec = shr_score_img_ptr->as_vector();
+
+      auto& n_adc_pixel = n_adc_pixel_v[plane];
+      auto& avg_adc_pixel_val = avg_adc_pixel_val_v[plane];
+      auto& std_adc_pixel_val = std_adc_pixel_val_v[plane];
+      auto& tot_adc_pixel_val = tot_adc_pixel_val_v[plane];
       
       auto& n_trk_pixel = n_trk_pixel_v[plane];
       auto& avg_trk_pixel_val = avg_trk_pixel_val_v[plane];
@@ -199,10 +224,6 @@ namespace larcv {
       auto& avg_shr_score = avg_shr_score_v[plane];
       auto& std_shr_score = std_shr_score_v[plane];
 
-      auto& n_adc_pixel = n_adc_pixel_v[plane];
-      auto& avg_adc_pixel_val = avg_adc_pixel_val_v[plane];
-      auto& std_adc_pixel_val = std_adc_pixel_val_v[plane];
-      auto& tot_adc_pixel_val = tot_adc_pixel_val_v[plane];
 
       //
       // avg
@@ -213,20 +234,33 @@ namespace larcv {
 	n_adc_pixel       += 1;
 	avg_adc_pixel_val += adc_vec[ix];
 	tot_adc_pixel_val += adc_vec[ix];
-      }
 
-      for(size_t ix=0; ix < shr_vec.size(); ++ix) {
 	if (trk_vec[ix] > _threshold) {
 	  n_trk_pixel       += 1;
 	  avg_trk_pixel_val += trk_vec[ix];
 	  tot_trk_pixel_val += trk_vec[ix];
-	  avg_trk_score     += trk_score_vec[ix];
 	}
+
 	if (shr_vec[ix] > _threshold)  {
 	  n_shr_pixel       += 1;
 	  avg_shr_pixel_val += shr_vec[ix];
 	  tot_shr_pixel_val += shr_vec[ix];
-	  avg_shr_score     += shr_score_vec[ix];
+	}
+
+      }
+
+      assert (shr_score_vec.size() == trk_score_vec.size());
+
+      size_t n_trk_score = 0;
+      size_t n_shr_score = 0;
+      for(size_t ix=0; ix < shr_score_vec.size(); ++ix) {
+	if (trk_score_vec[ix]>0) {
+	  avg_trk_score += trk_score_vec[ix];
+	  n_trk_score += 1;
+	}
+	if(shr_score_vec[ix]>0) {
+	  avg_shr_score += shr_score_vec[ix];
+	  n_shr_score += 1;
 	}
       }
 
@@ -234,40 +268,41 @@ namespace larcv {
       avg_trk_pixel_val /= n_trk_pixel;
       avg_shr_pixel_val /= n_shr_pixel;
 
-      avg_trk_score /= n_trk_pixel;
-      avg_shr_score /= n_shr_pixel;
+      avg_trk_score /= n_trk_score;
+      avg_shr_score /= n_shr_score;
 
       //
       // std
       //
       for(size_t ix=0; ix<adc_vec.size(); ++ix) {
 	if (adc_vec[ix] < _threshold) continue;
-	std_adc_pixel_val += adc_vec[ix] - avg_adc_pixel_val;
-	std_adc_pixel_val *= std_adc_pixel_val;
-      }
+	std_adc_pixel_val += std::pow(adc_vec[ix] - avg_adc_pixel_val,2);
 
-      for(size_t ix=0; ix < shr_vec.size(); ++ix) {
 	if (trk_vec[ix] > _threshold) {
-	  std_trk_pixel_val += trk_vec[ix] - avg_trk_pixel_val;
-	  std_trk_score     += trk_score_vec[ix] - avg_trk_score;
-	  std_trk_pixel_val *= std_trk_pixel_val;
-	  std_trk_score     *= std_trk_score;
+	  std_trk_pixel_val += std::pow(trk_vec[ix] - avg_trk_pixel_val,2);
 	}
 	if (shr_vec[ix] > _threshold)  {
-	  std_shr_pixel_val += shr_vec[ix] - avg_shr_pixel_val;
-	  std_shr_score     += shr_score_vec[ix] - avg_shr_score;
-	  std_shr_pixel_val *= std_shr_pixel_val;
-	  std_shr_score     *= std_shr_score;
+	  std_shr_pixel_val += std::pow(shr_vec[ix] - avg_shr_pixel_val,2);
 	}
+
       }
 
-      std_adc_pixel_val /= n_adc_pixel - 1;
+      for(size_t ix=0; ix < shr_score_vec.size(); ++ix) {
+	if (trk_score_vec[ix] > 0)
+	  std_trk_score += std::pow(trk_score_vec[ix] - avg_trk_score,2);
 
-      std_trk_pixel_val /= n_trk_pixel - 1;
-      std_shr_pixel_val /= n_shr_pixel - 1;
+	if (shr_score_vec[ix] > 0)
+	  std_shr_score += std::pow(shr_score_vec[ix] - avg_shr_score,2);
+      }
+      
 
-      std_trk_score /= n_trk_pixel - 1;
-      std_shr_score /= n_shr_pixel - 1;
+      std_adc_pixel_val /= (n_adc_pixel - 1);
+
+      std_trk_pixel_val /= (n_trk_pixel - 1);
+      std_shr_pixel_val /= (n_shr_pixel - 1);
+
+      std_trk_score /= (n_trk_score - 1);
+      std_shr_score /= (n_shr_score - 1);
 
       std_adc_pixel_val = std::sqrt(std_adc_pixel_val);
 
---
+++ app/LArOpenCVHandle/SingleROIFaker.h
@@ -0,0 +1,64 @@
+/**
+ * \file SingleROIFaker.h
+ *
+ * \ingroup Package_Name
+ * 
+ * \brief Class def header for a class SingleROIFaker
+ *
+ * @author vgenty
+ */
+
+/** \addtogroup Package_Name
+
+    @{*/
+#ifndef __SINGLEROIFAKER_H__
+#define __SINGLEROIFAKER_H__
+
+#include "Processor/ProcessBase.h"
+#include "Processor/ProcessFactory.h"
+namespace larcv {
+
+  /**
+     \class ProcessBase
+     User defined class SingleROIFaker ... these comments are used to generate
+     doxygen documentation!
+  */
+  class SingleROIFaker : public ProcessBase {
+
+  public:
+    
+    /// Default constructor
+    SingleROIFaker(const std::string name="SingleROIFaker");
+    
+    /// Default destructor
+    ~SingleROIFaker(){}
+
+    void configure(const PSet&);
+
+    void initialize();
+
+    bool process(IOManager& mgr);
+
+    void finalize();
+
+  };
+
+  /**
+     \class larcv::SingleROIFakerFactory
+     \brief A concrete factory class for larcv::SingleROIFaker
+  */
+  class SingleROIFakerProcessFactory : public ProcessFactoryBase {
+  public:
+    /// ctor
+    SingleROIFakerProcessFactory() { ProcessFactory::get().add_factory("SingleROIFaker",this); }
+    /// dtor
+    ~SingleROIFakerProcessFactory() {}
+    /// creation method
+    ProcessBase* create(const std::string instance_name) { return new SingleROIFaker(instance_name); }
+  };
+
+}
+
+#endif
+/** @} */ // end of doxygen group 
+
