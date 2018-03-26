f0f61ddf73ba53c7b958167be097386f127bc7e1
Victor Genty <vgenty@nevis.columbia.edu>
2018-03-26 13:06:39 -0400

    try this

--- app/LArOpenCVHandle/ana/rse/txt_dump/txt_dump.sh
+++ app/LArOpenCVHandle/ana/rse/txt_dump/txt_dump.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "python stage0_dump_txt.py $1 $2"
-python stage0_dump_txt.py $1 $2
+python ${DIR}/stage0_dump_txt.py $1 $2
 
 echo "python stage1_filter_larcv.py $3 $4"
-python stage1_filter_larcv.py $3 $4
+python ${DIR}/stage1_filter_larcv.py $3 $4
 
 echo "python stage2_hadd.py $5 $6"
-python stage2_hadd.py $5 $6
+python ${DIR}/stage2_hadd.py $5 $6
