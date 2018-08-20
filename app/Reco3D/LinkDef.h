e8bd5f2136377ce468d62cac613da2b74ab9f348
hourlier <hourlieradrien@gmail.com>
2018-08-20 10:35:24 -0500

    updated LinkDef to remove useless classes

--- app/Reco3D/LinkDef.h
+++ app/Reco3D/LinkDef.h
@@ -3,20 +3,20 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class larcv::AStar3DAlgoConfig+;
-#pragma link C++ class larcv::AStar3DAlgoProton+;
+//#pragma link C++ class larcv::AStar3DAlgoConfig+;
+//#pragma link C++ class larcv::AStar3DAlgoProton+;
 #pragma link C++ class larcv::AStarTracker+;
-#pragma link C++ class larcv::A3DPixPos_t+;
-#pragma link C++ class larcv::AStar3DNode+;
-#pragma link C++ class std::map< larcv::A3DPixPos_t, larcv::AStar3DNode* >+;
-#pragma link C++ class larcv::Lattice+;
-#pragma link C++ class std::vector< larcv::AStar3DNode* >+;
-#pragma link C++ class larcv::AStar3DNodePtrList+;
-#pragma link C++ class larcv::AStar3DAlgo+;
+//#pragma link C++ class larcv::A3DPixPos_t+;
+//#pragma link C++ class larcv::AStar3DNode+;
+//#pragma link C++ class std::map< larcv::A3DPixPos_t, larcv::AStar3DNode* >+;
+//#pragma link C++ class larcv::Lattice+;
+//#pragma link C++ class std::vector< larcv::AStar3DNode* >+;
+//#pragma link C++ class larcv::AStar3DNodePtrList+;
+//#pragma link C++ class larcv::AStar3DAlgo+;
 
 //#pragma link C++ class larcv::ReadRuiFile+;
-#pragma link C++ class larcv::ReadJarrettFile+;
-#pragma link C++ class larcv::ReadNueFile+;
+//#pragma link C++ class larcv::ReadJarrettFile+;
+//#pragma link C++ class larcv::ReadNueFile+;
 #pragma link C++ class larcv::Run3DTracker+;
 #pragma link C++ class larcv::TrackerEventDisplay+;
 
