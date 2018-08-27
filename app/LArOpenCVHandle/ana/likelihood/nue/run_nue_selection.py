bcb38109cc6b5d867836661609eb052579b31b30
Victor Genty <vgenty@nevis.columbia.edu>
2018-08-27 15:22:42 -0400

    push

--- app/LArOpenCVHandle/ana/likelihood/nue/run_nue_selection.py
+++ app/LArOpenCVHandle/ana/likelihood/nue/run_nue_selection.py
@@ -1,10 +1,11 @@
 import os, sys, gc
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
     print 
     print "COMB_DF = str(sys.argv[1])"
-    print "NUM     = str(sys.argv[2])"
-    print "OUTDIR  = str(sys.argv[3])"
+    print "DROP    = str(sys.argv[2])"
+    print "NUM     = str(sys.argv[3])"
+    print "OUTDIR  = str(sys.argv[4])"
     print 
     sys.exit(1)
 
@@ -17,12 +18,17 @@ BASE_PATH = os.path.dirname(BASE_PATH)
 sys.path.insert(0,BASE_PATH)
 
 COMB_DF = str(sys.argv[1])
-NUM     = str(sys.argv[2])
-OUTDIR  = str(sys.argv[3])
+DROP    = int(str(sys.argv[2]))
+NUM     = str(sys.argv[3])
+OUTDIR  = str(sys.argv[4])
 
 from lib.cuts import apply_cuts
+drop_list = None
+if DROP == 1:
+drop_lis
 
-cut_df = apply_cuts(COMB_DF)
+
+cut_df = apply_cuts(COMB_DF,drop_list=drop_list)
 
 out_file = os.path.join(OUTDIR,"comb_cut_df_%s.pkl" % NUM)
 cut_df.to_pickle(out_file)
