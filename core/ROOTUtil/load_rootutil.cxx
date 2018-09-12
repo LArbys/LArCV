891418736243abd081656e961ecd11674be938c9
twongjirad <taritree.wongjirad@gmail.com>
2018-09-11 23:44:50 -0400

    added rootutil

---
+++ core/ROOTUtil/load_rootutil.cxx
@@ -0,0 +1,12 @@
+#ifndef LOAD_ROOTUTIL_CXX
+#define LOAD_ROOTUTIL_CXX
+
+#include "load_rootutil.h"
+
+namespace larcv{
+
+  load_rootutil::load_rootutil(){}
+
+}
+
+#endif
---
+++ core/ROOTUtil/load_rootutil.h
@@ -0,0 +1,40 @@
+/**
+ * \file load_rootutil.h
+ *
+ * \ingroup Rootutil
+ * 
+ * \brief Class def header for a class load_rootutil
+ *
+ * @author kazuhiro
+ */
+
+/** \addtogroup Rootutil
+
+    @{*/
+#ifndef LOAD_ROOTUTIL_H
+#define LOAD_ROOTUTIL_H
+
+#include <iostream>
+
+namespace larcv {
+  /**
+     \class load_rootutil
+     User defined class load_rootutil ... these comments are used to generate
+     doxygen documentation!
+  */
+  class load_rootutil{
+    
+  public:
+    
+    /// Default constructor
+    load_rootutil();
+
+    /// Default destructor
+    ~load_rootutil(){}
+    
+  };
+}
+
+#endif
+/** @} */ // end of doxygen group 
+
