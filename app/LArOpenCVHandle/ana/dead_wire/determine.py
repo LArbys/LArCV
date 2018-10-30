777390a42218fa5d9239566d94af5ddd4dfc9c50
Victor Genty <vgenty@nevis.columbia.edu>
2018-10-30 09:51:16 -0400

    update determine

--- app/LArOpenCVHandle/ana/dead_wire/determine.py
+++ app/LArOpenCVHandle/ana/dead_wire/determine.py
@@ -1,9 +1,10 @@
 import os, sys
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print 
-    print "SSFILE  = str(sys.argv[1])"
-    print "OUTDIR  = str(sys.argv[2])" 
+    print "SUPERA = str(sys.argv[1])"
+    print "NUM    = str(sys.argv[2])"
+    print "OUTDIR = str(sys.argv[3])" 
     print 
     sys.exit(1)
 
@@ -13,19 +14,18 @@ BASE_PATH = os.path.realpath(__file__)
 BASE_PATH = os.path.dirname(BASE_PATH)
 sys.path.insert(0,BASE_PATH)
 
-SSFILE  = str(sys.argv[1])
-OUTDIR  = str(sys.argv[2])
-num = int(os.path.basename(SSFILE).split(".")[0].split("_")[-1])
+SUPERA  = str(sys.argv[1])
+NUM     = str(sys.argv[2])
+OUTDIR  = str(sys.argv[3])
 
-OUTFILE = "dead_wire_out_%d.root" % num
+OUTFILE = "dead_wire_out_%s.root" % NUM
 
 from larcv import larcv
 proc = larcv.ProcessDriver(ROOT.std.string("ProcessDriver"))
 cfg = ROOT.std.string(os.path.join(BASE_PATH,"determine.cfg"))
-print cfg
 proc.configure(cfg)
 flist_v = ROOT.std.vector("std::string")()
-flist_v.push_back(ROOT.std.string(SSFILE))
+flist_v.push_back(ROOT.std.string(SUPERA))
 proc.override_input_file(flist_v)
 proc.override_output_file(ROOT.std.string(os.path.join(OUTDIR,OUTFILE)))
 proc.override_ana_file(ROOT.std.string(os.path.join(OUTDIR,OUTFILE.replace("out","ana"))))
