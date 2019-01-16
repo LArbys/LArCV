feff36a4fc91f76784b4e421400fa3e000e9cd24
hourlier <hourlieradrien@gmail.com>
2019-01-16 10:11:59 -0600

    just added a runDisplay script for safekeeping

---
+++ app/Reco3D/runDisplay.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+setuBooNE
+
+DataPATH="/Volumes/DataStorage/DeepLearningData/MCC8_Overlays"
+SelectionPATH="/Users/hourlier/Documents/PostDocMIT/Research/MicroBooNE/FinalFiles/test20/overlays"
+#for run in {6860..7150}
+for run in {7014..7150}
+do
+    for subrun in {0..2000}
+    do
+        echo $run":"$subrun
+        printf -v larliteFile "tracker_reco_%04d%04d.root" "$run" "$subrun"
+        printf -v analysisFile "tracker_anaout_%04d%04d.root" "$run" "$subrun"
+        printf -v image2DFile "supera-Run%06d-SubRun%06d.root" "$run" "$subrun"
+        if [ -f  $DataPATH/$image2DFile ]; then
+            if [ -f $DataPATH/$larliteFile ]; then
+#                time python mac/run_display.py $DataPATH/$image2DFile $DataPATH/$larliteFile $DataPATH/$analysisFile $SelectionPATH/png $SelectionPATH/SelectedVerticesFile.txt
+                time python mac/run_display.py $DataPATH/$image2DFile $DataPATH/$larliteFile $DataPATH/$analysisFile $SelectionPATH/png4Jarrett $SelectionPATH/ThanksForMakingMePlotsAdrien.txt
+            fi
+        fi
+    done
+done
