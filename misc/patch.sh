055b66f5465d351600ec65321e2dff7552b1b045
twongjirad <taritree.wongjirad@gmail.com>
2019-01-30 00:27:25 -0500

    move torchutils

--- misc/patch.sh
+++ misc/patch.sh
@@ -11,6 +11,15 @@ sed -i 's|\"CPPUtil/|\"larcv/core/CPPUtil/|g' *.h
 sed -i 's|\"Processor/|\"larcv/core/Processor/|g' *.cxx
 sed -i 's|\"Processor/|\"larcv/core/Processor/|g' *.h
 
-sed -i 's|\"Processor/|\"larcv/core/ROOTUtil/|g' *.cxx
-sed -i 's|\"Processor/|\"larcv/core/ROOTUtil/|g' *.h
+sed -i 's|\"ROOTUtil/|\"larcv/core/ROOTUtil/|g' *.cxx
+sed -i 's|\"ROOTUtil/|\"larcv/core/ROOTUtil/|g' *.h
+
+sed -i 's|\"CVUtil/|\"larcv/core/CVUtil/|g' *.cxx
+sed -i 's|\"CVUtil/|\"larcv/core/CVUtil/|g' *.h
+
+sed -i 's|\"PyUtil/|\"larcv/core/PyUtil/|g' *.cxx
+sed -i 's|\"PyUtil/|\"larcv/core/PyUtil/|g' *.h
+
+sed -i 's|\"TorchUtil/|\"larcv/core/TorchUtil/|g' *.cxx
+sed -i 's|\"TorchUtil/|\"larcv/core/TorchUtil/|g' *.h
 
