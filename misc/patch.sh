c786ed8e1088af4aa1faf35a8571d70614353101
twongjirad <taritree.wongjirad@gmail.com>
2019-01-29 23:51:22 -0500

    added patch script for headers

---
+++ misc/patch.sh
@@ -0,0 +1,16 @@
+
+sed -i 's|\"Base/|\"larcv/core/Base/|g' *.cxx
+sed -i 's|\"Base/|\"larcv/core/Base/|g' *.h
+
+sed -i 's|\"DataFormat/|\"larcv/core/DataFormat/|g' *.cxx
+sed -i 's|\"DataFormat/|\"larcv/core/DataFormat/|g' *.h
+
+sed -i 's|\"CPPUtil/|\"larcv/core/CPPUtil/|g' *.cxx
+sed -i 's|\"CPPUtil/|\"larcv/core/CPPUtil/|g' *.h
+
+sed -i 's|\"Processor/|\"larcv/core/Processor/|g' *.cxx
+sed -i 's|\"Processor/|\"larcv/core/Processor/|g' *.h
+
+sed -i 's|\"Processor/|\"larcv/core/ROOTUtil/|g' *.cxx
+sed -i 's|\"Processor/|\"larcv/core/ROOTUtil/|g' *.h
+
