4d073b5c00f5d4c3f7f4debd0bfbc91c8d76971e
twongjirad <taritree.wongjirad@gmail.com>
2019-01-31 00:34:06 -0500

    fixed up optional geo2d dependency

--- configure.sh
+++ configure.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Use this to not build supera (can cause conflicts when building supera in larsoft)
+#export LARCV_NOSUPERA=1
+
+# Use this to not build w/ opencv
+#export LARCV_NOOPENCV=1
+
 # clean up previously set env
 if [[ -z $FORCE_LARCV_BASEDIR ]]; then
     where="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -7,21 +13,6 @@ if [[ -z $FORCE_LARCV_BASEDIR ]]; then
 else
     export LARCV_BASEDIR=$FORCE_LARCV_BASEDIR
 fi
-
-# set the version
-export LARCV_VERSION=1
-
-# set the build dir
-unset LARCV_BUILDDIR
-if [[ -z $LARCV_BUILDDIR ]]; then
-    export LARCV_BUILDDIR=$LARCV_BASEDIR/build
-fi
-
-export LARCV_COREDIR=$LARCV_BASEDIR/larcv/core
-export LARCV_APPDIR=$LARCV_BASEDIR/larcv/app
-export LARCV_LIBDIR=$LARCV_BUILDDIR/installed/lib
-export LARCV_INCDIR=$LARCV_BUILDDIR/installed/include
-export LARCV_BINDIR=$LARCV_BUILDDIR/installed/bin
 
 # Abort if ROOT not installed. Let's check rootcint for this.
 if [ `command -v rootcling` ]; then
@@ -36,6 +27,35 @@ else
 	return 1;
     fi
 fi
+
+# check for either clang or gcc
+export LARCV_CXX=clang++
+if [ -z `command -v $LARCV_CXX` ]; then
+    export LARCV_CXX=g++
+    if [ -z `command -v $LARCV_CXX` ]; then
+        echo
+        echo Looks like you do not have neither clang or g++!
+        echo You need one of those to compile LArCaffe... Abort config...
+        echo
+        return 1;
+    fi
+fi
+
+# set the build dir
+unset LARCV_BUILDDIR
+if [[ -z $LARCV_BUILDDIR ]]; then
+    export LARCV_BUILDDIR=$LARCV_BASEDIR/build
+fi
+
+# set important directories
+export LARCV_COREDIR=$LARCV_BASEDIR/larcv/core
+export LARCV_APPDIR=$LARCV_BASEDIR/larcv/app
+export LARCV_LIBDIR=$LARCV_BUILDDIR/installed/lib
+export LARCV_INCDIR=$LARCV_BUILDDIR/installed/include
+export LARCV_BINDIR=$LARCV_BUILDDIR/installed/bin
+
+# set the version
+export LARCV_VERSION=1
 
 # Check OpenCV
 export LARCV_OPENCV=1
@@ -96,76 +116,23 @@ printf "    \033[95mLARCV_INCDIR\033[00m   = $LARCV_INCDIR\n"
 printf "    \033[95mLARCV_LIBDIR\033[00m   = $LARCV_LIBDIR\n"
 printf "    \033[95mLARCV_BUILDDIR\033[00m = $LARCV_BUILDDIR\n"
 
-export PATH=$LARCV_BASEDIR/bin:$LARCV_BINDIR:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LARCV_LIBDIR
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$LARCV_LIBDIR
+# modify paths
+[[ ":$PATH:" != *":${LARCV_BASEDIR}/bin:"* ]] && export PATH="${LARCV_BASEDIR}/bin:${PATH}"
+[[ ":$LD_LIBRARY_PATH:" != *":${LARCV_LIBDIR}/bin:"* ]] && export LD_LIBRARY_PATH="${LARCV_LIBDIR}:${LD_LIBRARY_PATH}"
+[[ ":$DYLD_LIBRARY_PATH:" != *":${LARCV_LIBDIR}/bin:"* ]] && export DYLD_LIBRARY_PATH="${LARCV_LIBDIR}:${DYLD_LIBRARY_PATH}"
 
-if [[ -z $NO_OPENCV ]]; then
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENCV_LIBDIR
-    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$OPENCV_LIBDIR
+# paths if using OPENCV (NO_OPENCV to explicitly disallow)
+if [[ -z $LARCV_NOOPENCV ]]; then
+    [[ ":$LD_LIBRARY_PATH:" != *":${OPENCV_LIBDIR}/bin:"* ]] && export LD_LIBRARY_PATH="${OPENCV_LIBDIR}:${LD_LIBRARY_PATH}"
+    [[ ":$DYLD_LIBRARY_PATH:" != *":${OPENCV_LIBDIR}/bin:"* ]] && export DYLD_LIBRARY_PATH="${OPENCV_LIBDIR}:${DYLD_LIBRARY_PATH}"    
 fi
 
 mkdir -p $LARCV_BUILDDIR;
 mkdir -p $LARCV_LIBDIR;
 mkdir -p $LARCV_BINDIR;
 
-export LD_LIBRARY_PATH=$LARCV_LIBDIR:$LD_LIBRARY_PATH
-export PYTHONPATH=$LARCV_BASEDIR/python:$PYTHONPATH
-
-if [[ $LARLITE_BASEDIR ]]; then
-    printf "\033[93mLArLite\033[00m\n"
-    echo "    Found larlite set up @ \$LARLITE_BASEDIR=${LARLITE_BASEDIR}"
-    echo "    Preparing APILArLite package for build (making sym links)"
-    target=$LARCV_APPDIR/Supera/larfmwk_shared/*
-    for f in $target
-    do
-	ln -sf $f $LARCV_APPDIR/Supera/APILArLite/
-    done
-fi
-
-if [[ -d $MRB_TOP/srcs/uboonecode/uboone ]]; then
-    printf "\033[93mLArSoft\033[00m\n"
-    echo "    Found local larsoft @ \$MRB_TOP=${MRB_TOP}"
-    echo "    Preparing APILArSoft package for build (making sym links)"
-    target=$LARCV_APPDIR/Supera/larfmwk_shared/*
-    for f in $target
-    do
-	ln -sf $f $LARCV_APPDIR/Supera/APILArSoft/
-    done
-    if [ ! -d $MRB_TOP/srcs/uboonecode/uboone/Supera ]; then
-	ln -s $LARCV_APPDIR/Supera/APILArSoft $MRB_TOP/srcs/uboonecode/uboone/Supera
-    fi
-fi
-
-if [[ -d $MRB_TOP/srcs/argoneutcode/ ]]; then
-    printf "\033[93mLArSoft\033[00m\n"
-    echo "    Found local larsoft @ \$MRB_TOP=${MRB_TOP}"
-    echo "    Preparing APILArSoft package for build (making sym links)"
-    target=$LARCV_APPDIR/Supera/larfmwk_shared/*
-    for f in $target
-    do
-    ln -sf $f $LARCV_APPDIR/Supera/APILArSoft/
-    done
-    if [ ! -d $MRB_TOP/srcs/argoneutcode/Supera ]; then
-    ln -s $LARCV_APPDIR/Supera/APILArSoft $MRB_TOP/srcs/argoneutcode/Supera
-    fi
-fi
-
-
-export LARCV_CXX=clang++
-if [ -z `command -v $LARCV_CXX` ]; then
-    export LARCV_CXX=g++
-    if [ -z `command -v $LARCV_CXX` ]; then
-        echo
-        echo Looks like you do not have neither clang or g++!
-        echo You need one of those to compile LArCaffe... Abort config...
-        echo
-        return 1;
-    fi
-fi
-
-# Use this to not build supera (can cause conflicts when building supera in larsoft)
-#export LARCV_NOSUPERA=1
+# add to python path
+[[ ":$PYTHONPATH:" != *":${LARCV_BASEDIR}/python:"* ]] && export PYTHONPATH="${LARCV_BASEDIR}/python:${PATH}"
 
 echo
 echo "Finish configuration. To build, type:"
