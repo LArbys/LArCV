29c540960df865a597dc1fb6bb7e449305605688
twongjirad <taritree.wongjirad@gmail.com>
2019-03-08 17:33:41 -0500

    add pixellist json

--- larcv/core/json/json_utils.h
+++ larcv/core/json/json_utils.h
@@ -24,6 +24,17 @@ namespace larcv {
     larcv::Image2D image2d_from_bson( const std::vector<std::uint8_t>& b );
     larcv::Image2D image2d_from_json_str( const std::string& s );    
 
+    json as_json_pixelarray( const larcv::Image2D&, const float );
+    json as_json_pixelarray_withselection( const larcv::Image2D&,
+                                           const larcv::Image2D&,
+                                           const float );
+    std::vector<std::uint8_t> as_bson_pixelarray( const larcv::Image2D& img,
+                                                  const float threshold );
+    std::vector<std::uint8_t> as_bson_pixelarray_withselection( const larcv::Image2D& img,
+                                                                const larcv::Image2D& selectimg,
+                                                                const float threshold );
+    larcv::Image2D image2d_from_bson_pixelarray( const std::vector<std::uint8_t>& bson );
+   
 #ifdef HASPYUTIL
     PyObject* as_pystring( const larcv::Image2D& img );
     larcv::Image2D image2d_from_pystring( PyObject* str );
